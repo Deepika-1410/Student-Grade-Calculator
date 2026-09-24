3fe2372e8de2a7ac54a3866606b80af1bd330e4d
Deepika-1410 <deepikasadhasivam2007@gmail.com>
2026-09-24 11:59:17 +0530

    Add grade calculator

---
+++ main.py
@@ -0,0 +1,27 @@
+name = input("Enter student name: ")
+
+mark1 = int(input("Enter mark 1: "))
+mark2 = int(input("Enter mark 2: "))
+mark3 = int(input("Enter mark 3: "))
+
+total = mark1 + mark2 + mark3
+average = total / 3
+
+if average >= 90:
+    grade = "A+"
+elif average >= 80:
+    grade = "A"
+elif average >= 70:
+    grade = "B"
+elif average >= 60:
+    grade = "C"
+elif average >= 50:
+    grade = "D"
+else:
+    grade = "F"
+
+print("\n--- Student Result ---")
+print("Name:", name)
+print("Total:", total)
+print("Average:", average)
+print("Grade:", grade)

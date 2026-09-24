48e52accba4cd4ad24ceff899c047a9521950c86
Deepika-1410 <deepikasadhasivam2007@gmail.com>
2026-09-24 12:04:22 +0530

    Add pass fail status

--- main.py
+++ main.py
@@ -19,9 +19,14 @@ elif average >= 50:
     grade = "D"
 else:
     grade = "F"
+if average >= 50:
+    result = "Pass"
+else:
+    result = "Fail"
 
 print("\n--- Student Result ---")
 print("Name:", name)
 print("Total:", total)
 print("Average:", average)
 print("Grade:", grade)
+print("Result:", result)
